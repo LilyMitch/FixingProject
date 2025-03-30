bf344649c180a3effd0b7340071af03e3bc3ea72
LilyMitch <duncanm030308@outlook.com>
2025-03-30 12:55:25 -0400

    Update Source.cpp
    Fixed

--- BadMath/Source.cpp
+++ BadMath/Source.cpp
@@ -4,7 +4,11 @@ using namespace std;
 
 float average(int i1, int i2, int i3)
 {
-	return i1 + i2 + i3 / 3.0f;
+	float a = 0;
+	a = (i1 + i2 + i3); //for some reason this started by eqauling 2 when all numbers were 1
+	//cout << "a = " << a << endl; used to find error
+	a = a / 3;
+	return a;
 }
 
 int main()
@@ -13,11 +17,16 @@ int main()
 	cout << "This program calculates the average of three numbers." << endl;
 	cout << "First number: "; cin >> n1;
 	cout << "Second number: "; cin >> n2;
-	cout << "Third number: "; cin >> n2;
+	cout << "Third number: "; cin >> n3; // was n2 instead of n3
 
-	float a = average(n1, n2, n3);
+	float a = 0;
+	a = average(n1, n2, n3); // At first makes it equal the total instead
 
-	cout << setprecision(1) << fixed << "The average is " << a << endl;
+	cout << setprecision(2) << fixed << "The average is " << a << endl; // Not an error but I wanted to see an extra decimal
 
+
+	cout << "\n\nPress enter to close the program\n";
+	cin.ignore();
+	cin.get(); //Now th program dosen't just close when not in debug mode before answer is shown
 	return 0;
 }
